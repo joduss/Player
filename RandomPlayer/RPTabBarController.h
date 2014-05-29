e6d3c0c12bdf1e185daca9051ecd0c6060d5c359
Jonathan Duss <jonathan.duss@epfl.ch>
2014-05-29 17:09:54 +0200

    Add Swipe to show queue view controller

---
+++ RandomPlayer/RPTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  RPTabBarController.h
+//  RandomPlayer
+//
+//  Created by Jonathan Duss on 29.05.14.
+//  Copyright (c) 2014 Jonathan Duss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RPTabBarController : UITabBarController
+
+@end

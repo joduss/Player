eb2ba104611932c42056cf9c88c628b46d87c58b
Jonathan Duss <jonathan.duss@tapptic.com>
2017-10-11 23:38:02 +0200

    pods update

---
+++ RandomPlayer/MPMediaItemCollectionExtension.swift
@@ -0,0 +1,28 @@
+//
+//  MediamItemExtension.swift
+//  ClassicPlayer
+//
+//  Created by Jonathan Duss on 11.10.17.
+//  Copyright © 2017 Jonathan Duss. All rights reserved.
+//
+
+import Foundation
+
+
+
+//extension that adds an easy way to have the count of different albums
+extension MPMediaItemCollection {
+    var albumCount : Int {
+        get {
+            let query = MPMediaQuery.albums()
+            let filterPredicate = MPMediaPropertyPredicate(
+                value: representativeItem!.value(forProperty: MPMediaItemPropertyArtistPersistentID),
+                forProperty: MPMediaItemPropertyArtistPersistentID)
+            query.filterPredicates = Set(arrayLiteral: filterPredicate)
+            
+            return query.collections!.count
+        }
+    }
+    
+    
+}
---
+++ RandomPlayer/Utility/Extensions/ArrayExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ArrayExtension.swift
+//  ClassicPlayer
+//
+//  Created by Jonathan Duss on 11.10.17.
+//  Copyright © 2017 Jonathan Duss. All rights reserved.
+//
+
+import Foundation
